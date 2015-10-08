b537d860e5e7ddf61128f3dc75a03fa9a397a729
Rajit Singh <rajit.b.singh@gmail.com>
2015-10-08 08:39:24 +0100

    First commit

---
+++ bootstrap3-datepicker-example.js
@@ -0,0 +1,29 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  Template.datepicker.onRendered(function () {
+    $('#iamthedatepicker' + this.data.language).datepicker({
+      language: this.data.language
+    });
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
